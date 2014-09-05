3ebb79b3d14ff9956669a4a76c01a6a6fc0eaad4
Rodrigo Medina <rckdrigomed@gmail.com>
2014-09-05 13:18:30 -0500

    Splash Screen

---
+++ Assets/SplashImage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode()]
+public class SplashImage : MonoBehaviour {
+	public Texture wallpaper;
+	public GUIStyle style;
+
+	void OnGUI(){
+		GUI.color = new Color(0.7265625f,0.42578125f,0.5625f,0.7f);
+		GUI.DrawTexture (new Rect(0,0,Screen.width,Screen.height),wallpaper);
+		GUILayout.BeginArea (new Rect(Screen.width/2-40,0,80,300));
+			GUILayout.Label ("Cuando se active tu camara, enfoca la imagen del COLLARIN Y DEJALA ENFOCADA UNOS ",style);
+		GUILayout.EndArea ();
+	}
+}
