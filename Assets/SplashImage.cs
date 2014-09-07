cf044b124900832987d479ca21a080cbc1152ce5
Rckdrigo <rckdrigomed@gmail.com>
2014-09-06 20:43:44 -0500

    Splash Screen Sprite

--- Assets/SplashImage.cs
+++ Assets/SplashImage.cs
@@ -7,10 +7,11 @@ public class SplashImage : MonoBehaviour {
 	public GUIStyle style;
 
 	void OnGUI(){
-		GUI.color = new Color(0.7265625f,0.42578125f,0.5625f,0.7f);
+        GUI.color = new Color(0.9019607843137255f, 0.5215686274509804f, 0.6980392156862745f);
+		//GUI.color = new Color(0.7265625f,0.42578125f,0.5625f,0.7f); //ROSA
 		GUI.DrawTexture (new Rect(0,0,Screen.width,Screen.height),wallpaper);
 		GUILayout.BeginArea (new Rect(Screen.width/2-40,0,80,300));
-			GUILayout.Label ("Cuando se active tu camara, enfoca la imagen del COLLARIN Y DEJALA ENFOCADA UNOS ",style);
+		//	GUILayout.Label ("Cuando se active tu camara, enfoca la imagen del COLLARIN Y DEJALA ENFOCADA UNOS ",style);
 		GUILayout.EndArea ();
 	}
 }
---
+++ Assets/StartButton.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+    void OnMouseDown() {
+        print("START");
+    }
+}
