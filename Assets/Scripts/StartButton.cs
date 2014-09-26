cfd33785aed4697993a913d20c86fcb7eef9f14a
Rodrigo Medina <rckdrigomed@gmail.com>
2014-09-26 15:56:38 -0500

    New screens

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -8,19 +8,7 @@ using UnityEngine;
 using System.Collections;
 
 public class StartButton : MonoBehaviour {
-    public GameObject[] steps;
-    public GameObject splash, inicial;
-
-    void OnMouseDown()
-    {
-        Destroy(splash);
-        Destroy(inicial);
-
-        foreach (GameObject step in steps)
-            step.GetComponent<StepChanger>().changeable = true;
-
-        Destroy(gameObject);
-    }
+    
 
 
 }
---
+++ Assets/Scripts/UIScreens.cs
@@ -0,0 +1,49 @@
+﻿/*==============================================================================
+*	Created by Rodrigo Medina 
+*				Sept / 2014	
+*	Github: Rckdrigo
+==============================================================================*/
+
+using UnityEngine;
+using System.Collections;
+
+public class UIScreens : MonoBehaviour {
+	public GameObject[] steps;
+    public GameObject aRCam;
+
+	Animator anim;
+
+	void Start(){
+		anim = GetComponent<Animator> ();
+	}
+
+
+    void startAR(){
+        aRCam.SetActive(true);
+    }
+
+	void Update(){
+		if (Input.GetKey(KeyCode.Escape))
+			Application.Quit ();
+
+		
+		if (anim.GetCurrentAnimatorStateInfo (0).IsName ("NoScreen"))
+			collider.enabled = false;
+	}
+
+	void OnApplicationPause(bool pause){
+		if(pause)
+			Application.Quit ();
+	}
+
+	void OnMouseDown(){
+			anim.SetTrigger ("NextScreen");
+	}
+
+	void activateSteps(){
+		foreach (GameObject step in steps)
+			step.GetComponent<StepChanger>().changeable = true;
+	}
+	
+		
+}
